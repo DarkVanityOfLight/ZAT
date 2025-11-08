36d1fd161d81576d3bd3de515e12c094b85a17eb
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2025-11-08 13:09:05 +0100

    chore: ...

--- src/main.zig
+++ src/main.zig
@@ -1,27 +1,20 @@
 const std = @import("std");
 const ZAT = @import("ZAT");
 
+const Clauses = @import("clauses.zig");
+const DIMACS = @import("DIMACS_parser.zig");
+
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
     try ZAT.bufferedPrint();
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+test "parse dimacs" {
+    const allocator = std.testing.allocator;
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    const t = "p cnf 2 2\nc foo \n1 0\n-1\n";
+    const cnf = try DIMACS.parse_dimacs(allocator, t);
+    cnf.deinit();
+    allocator.destroy(cnf);
 }
