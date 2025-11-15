f6e8b258d571dc4237fa56e2541bca2a4253ad78
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2025-11-15 22:51:01 +0100

    chore: Add the new things to ZAT lib

--- src/root.zig
+++ src/root.zig
@@ -4,3 +4,6 @@ const std = @import("std");
 pub const DIMACS = @import("DIMACS_parser.zig");
 pub const Clauses = @import("clauses.zig");
 pub const Variables = @import("variables.zig");
+pub const Bank = @import("bank.zig");
+pub const DPLL = @import("dpll.zig");
+pub const LiteralEpochDict = @import("datastructures/LiteralEpochDict.zig").LiteralEpochDict;
